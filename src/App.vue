076e7aefbcafc27f90d137ddc10acc8c7ed1c7c0
qvarkk <xumdes@gmail.com>
2025-09-28 16:28:18 +0500

    modified app component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,64 @@
 <script lang="ts">
 import { useAuthStore } from '@/stores/authStore.ts';
 import type { StoreDefinition } from 'pinia'
+import Button from 'primevue/button';
+import Menubar from 'primevue/menubar';
+import InputText from 'primevue/inputtext';
+import type { User } from '@/services/authService.ts'
+
+interface MenubarItem {
+  label: string;
+  icon: string;
+  route: string;
+}
 
 export default {
+  components: {Button, Menubar, InputText},
   data(): {
     email: string,
     password: string,
     authStore: StoreDefinition
+    items: MenubarItem[]
   } {
     return {
       email: '',
       password: '',
       authStore: useAuthStore(),
+      items: [
+        {
+          label: 'Home',
+          icon: 'pi pi-fw pi-home',
+          route: '/',
+        },
+        {
+          label: 'Deals',
+          icon: 'pi pi-fw pi-money-bill',
+          route: '/deals'
+        },
+        {
+          label: 'Products',
+          icon: 'pi pi-fw pi-objects-column',
+          route: '/products'
+        },
+      ],
     }
   },
   computed: {
-    isAuthenticated() {
+    isAuthenticated(): boolean {
       return this.authStore.isAuthenticated;
     },
-    user() {
+    user(): User {
       return this.authStore.user;
     },
-    authError() {
+    authError(): string {
       return this.authStore.errorMessage;
     }
   },
   methods: {
-    logout() {
+    logout(): void {
       this.authStore.logout();
     },
-    login() {
+    login(): void {
       this.authStore.login(this.email, this.password);
     },
   },
@@ -44,33 +73,39 @@ export default {
 </script>
 
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li><router-link to="/">Главная</router-link></li>
-        <li><router-link to="/deals">Сделки</router-link></li>
-        <li><router-link to="/products">Товары</router-link></li>
-      </ul>
+  <Menubar :model="items">
+    <template #start>
+      <span>
+        <img src="@/assets/logo.svg" width="50" alt="Icon" />
+      </span>
+    </template>
+    <template #item="{ item, props, hasSubmenu, root }">
+      <a class="flex items-center ml-6 p-4">
+        <router-link v-if="item.route" :to="item.route">
+          <span :class="item.icon" />
+          <span class="ml-1">{{ item.label }}</span>
+        </router-link>
+      </a>
+    </template>
+    <template #end>
       <div v-if="isAuthenticated && user">
-        Welcome, {{ user.first_name }}
-        <button @click="logout">Logout</button>
+        <span class="pi pi-fw pi-user mr-4" /> {{ user.first_name }}
+        <Button @click="logout" class="ml-4">Logout</Button>
       </div>
       <div v-else>
         <form @submit.prevent="login">
-          <div>
-            <label for="email">Email:</label>
-            <input v-model="email" type="email" id="email" required />
-          </div>
-          <div>
-            <label for="email">Password:</label>
-            <input v-model="password" type="password" id="password" required />
-          </div>
-          <button type="submit">Login</button>
+          <InputText v-model="email" type="email" id="email" required
+                     placeholder="Email" class="m-2 sm:w-auto"
+                     :class="{'p-invalid': authError}" />
+          <InputText v-model="password" type="password" id="password"
+                     required placeholder="Password" class="m-2 sm:w-auto"
+                     :class="{'p-invalid': authError}" />
+          <Button type="submit">Login</Button>
           <p v-if="authError" class="error">{{ authError }}</p>
         </form>
       </div>
-    </nav>
-  </header>
+    </template>
+  </Menubar>
   <router-view></router-view>
 </template>
 
